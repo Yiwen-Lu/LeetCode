320e43cc1ade5c47f28d49f393690cbea3745adb
Yiwen <35602732+Yiwen-Lu@users.noreply.github.com>
2020-08-12 11:29:16 +0200

    Update 283_Move_Zeroes.py

--- Python/283_Move_Zeroes.py
+++ Python/283_Move_Zeroes.py
@@ -4,13 +4,21 @@ class Solution(object):
         :type nums: List[int]
         :rtype: None Do not return anything, modify nums in-place instead.
         """
-        count = 0
-        for i in nums:
-            if i == 0:
-                count += 1
+        # Method 1:
+        count = nums.count(0)
         
         for _ in range(count):
             nums.remove(0)
             nums.append(0)
             
         return nums
+    
+        """
+        # Method 2:
+        for i in nums:
+            if i == 0:
+                nums.remove(0)
+                nums.append(0)
+        """
+    
+    
